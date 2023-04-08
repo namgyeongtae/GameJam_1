57bc20c1e34611ce024617716b926b772066d19b
namgyeongtae <nrudxo12@kookmin.ac.kr>
2023-04-08 13:28:16 +0900

    이미지 로드 기능 추가
    TextureManager : 주어진 경로의 이미즈를 로드하여 renderer에게 전달하고 window에 뿌려주게끔 하는 역할

--- GameJamPro/GameJamPro/Engine.cpp
+++ GameJamPro/GameJamPro/Engine.cpp
@@ -1,7 +1,6 @@
 #include "Engine.h"
-#include <iostream>
-
-using namespace std;
+#include "TextureManager.h"
+#include "Transform.h"
 
 Engine* Engine::s_Instance = nullptr;
 
@@ -13,27 +12,65 @@ bool Engine::Init()
 		return false;
 	}
 
+	m_Window = SDL_CreateWindow("Soft Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
+								SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+	if(m_Window == nullptr) {
+		SDL_Log("Failed to create Window: %s", SDL_GetError());
+		return false;
+	}
+
+	m_Renderer = SDL_CreateRenderer(m_Window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (m_Renderer == nullptr)
+	{
+		SDL_Log("Failed to create Renderer: %s", SDL_GetError());
+		return false;
+	}
+
+	TextureManager::GetInstance()->Load("bomb_1", "Assets/bomb_1.png");
+
+	/*Transform tf;
+	tf.Log();*/
 	return m_IsRunning = true;
 }
 
 bool Engine::Clean()
 {
-	return false;
+	TextureManager::GetInstance()->Clean();
+	SDL_DestroyRenderer(m_Renderer);
+	SDL_DestroyWindow(m_Window);
+
+	IMG_Quit();
+	SDL_Quit();
+
+	return true;
 }
 
 void Engine::Quit()
 {
+	m_IsRunning = false;
 }
 
 void Engine::Update(float dt)
 {
-	SDL_Log("didididi");
 }
 
 void Engine::Render()
 {
+	SDL_SetRenderDrawColor(m_Renderer, 124, 218, 254, 255);
+	SDL_RenderClear(m_Renderer);
+
+	TextureManager::GetInstance()->Draw("bomb_1", 100, 100, 750, 683);
+	SDL_RenderPresent(m_Renderer);
 }
 
 void Engine::Events()
 {
+	SDL_Event event;
+	SDL_PollEvent(&event);
+	switch (event.type)
+	{
+		case SDL_QUIT:
+			Quit();
+			break;
+	}
 }
